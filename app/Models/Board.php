e2bfc57dff50aa87b8c34f37ba95342bff9d798d
deadman360 <vmartinho.kfg@gmail.com>
2025-11-28 18:08:39 -0300

    fixed typo in Board model

--- app/Models/Board.php
+++ app/Models/Board.php
@@ -4,9 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Board extends Model
 {
     /** @use HasFactory<\Database\Factories\BoardFactory> */
     use HasFactory;
+
+    protected $guarded = [
+        'id',
+    ];
+    protected $fillable = [
+        'name',
+        'description',
+        'created_by_id',
+    ];
+
+    public function createdBy() : BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by_id');
+    }
 }
--- database/migrations/2025_11_27_000210_create_boards_table.php
+++ database/migrations/2025_11_27_000210_create_boards_table.php
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('boards', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->foreignId('created_by_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
+
         });
     }
 
