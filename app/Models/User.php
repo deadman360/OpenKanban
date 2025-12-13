1a1b3bbaf321ea87c7c2c90086d567cd18a8e4d3
deadman360 <vmartinho.kfg@gmail.com>
2025-12-13 15:00:45 -0300

    project restart

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,8 +4,6 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -46,18 +44,5 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
-    }
-
-    public function tasks() : HasMany
-    {
-        return $this->hasMany(Task::class, 'assigned_to_id');
-    }
-
-    public function boards() : BelongsToMany
-    {
-        return $this->belongsToMany(Board::class)
-            ->using(BoardMember::class)
-            ->withPivot(['role'])
-            ->withTimestamps();
     }
 }
